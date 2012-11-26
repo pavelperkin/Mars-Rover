e7e9925258d65c4ae92ee8c27a95caddd828f0cd
Pavel <perkinpv@gmail.com>
2012-11-26 23:49:00 +0200

    add minitest

--- mars.rb
+++ mars.rb
@@ -1,22 +1,11 @@
-#  1 - up
-#  2- right
-#  4 - down
-#  8- left
+#  1 - up  2- right  4 - down   8- left
 require 'ostruct'
+
 class Position <OpenStruct
 end
 
-def start_position
-puts 'input a start position:'
-Position.new({:x => gets.to_i , :y => gets.to_i, :course => 1})
-end
-
-def get_commands
- IO.read('route.txt')
-end
-
-def parse_commands (s)
-  s.split
+def get_commands(com_file)
+ IO.read(com_file).split
 end
 
 def steps(route, current_pos)
@@ -54,15 +43,53 @@ def c_shift_left_1(a)
   a &= 15  
 end
 
-def start
-  current_pos=start_position
-  route=parse_commands(get_commands)
+def start( x , y, com_file)
+  current_pos=Position.new({:x => x, :y => y, :course => 1})
+  route=get_commands (com_file) 
   if (current_pos.x >= 0) && (current_pos.y >= 0) then
-    puts result=steps(route, current_pos)
+    result=steps(route, current_pos)
   else 
-    puts 'Position should be positive'
+    'Position should be positive'
   end
-  gets
 end
 
-start
+#----------------------------   Test   --------------------------------------
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe 'start' do
+  it 'should return string when x or y is negative' do
+    start( -1, 2, 'route.txt').must_equal 'Position should be positive'
+    start( 2, -1, 'route.txt').must_equal 'Position should be positive'
+    start( -1, -1, 'route.txt').must_equal 'Position should be positive'
+  end
+  
+   it 'should work correct' do
+      start(1, 1, 'route.txt').must_equal Position.new({:x => 1, :y => 1, :course => 1 })
+      start(1, 1, 'route1.txt').must_equal Position.new({:x => 5, :y => 6, :course => 2 })    
+      start(4, 4, 'route2.txt').must_equal Position.new({:x => 4, :y => 0, :course => 4 })    
+      start(4, 4, 'route3.txt').must_equal Position.new({:x => 0, :y => 4, :course => 8 })    
+  end   
+end
+
+describe 'c_shift_left_1' do
+  it 'should make cycle shift 4bit-number for one bit left' do
+      c_shift_left_1(0).must_equal(0)
+      c_shift_left_1(1).must_equal(2)
+      c_shift_left_1(8).must_equal(1)
+  end
+end
+
+describe 'c_shift_right_1' do
+  it 'should make cycle shift 4bit-number for one bit right' do
+      c_shift_right_1(0).must_equal(0)
+      c_shift_right_1(4).must_equal(2)
+      c_shift_right_1(1).must_equal(8)
+  end
+end
+
+describe 'get_commands' do
+  it 'should get and parse input file by space' do
+    get_commands('test_file.txt').must_equal(['This', 'is', 'a', 'test', 'string'])
+  end
+end  
